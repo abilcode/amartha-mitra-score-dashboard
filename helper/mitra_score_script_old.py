23cfab7615efedbe7ba057d9a4f69aae282eb64a
abilcode <m.nabilfad@gmail.com>
2023-12-03 16:05:16 +0700

    adding comment

--- helper/mitra_score_script_old.py
+++ helper/mitra_score_script_old.py
@@ -2,6 +2,19 @@ import pandas as pd
 import numpy as np
 
 def calculate_mitra_score(data, threshold = None, weight = None):
+    """
+       Calculate Mitra Score for the given data based on specified thresholds and weights.
+
+       Parameters:
+       - data (DataFrame): Input data containing various features.
+       - threshold (dict, optional): Dictionary specifying feature-specific threshold values.
+         If not provided, default quantile-based thresholds will be calculated from the input data.
+       - weight (dict, optional): Dictionary specifying feature-specific weights.
+         If not provided, default weights of 1 for each feature will be used.
+
+       Returns:
+       - aggregate (DataFrame): Aggregated Mitra Scores with corresponding label mean.
+       """
     df_ready = data
 
     # threshold = {
@@ -40,14 +53,15 @@ def calculate_mitra_score(data, threshold = None, weight = None):
     df_ready['total_saving_frequency'] = df_ready['total_saving_frequency'].fillna(0)
 
     df_score = df_ready.copy()
+    # Calculate scores for each feature based on thresholds and weights
     for c in weight.keys():
       df_score[f"score_{c}"] = df_score[c].apply(lambda x: 0 if x > threshold[c] else 1 * weight[c])
 
+    # Calculate the overall Mitra Score for each row (weighted sum)
     df_score['mitra_score'] = df_score.apply(
         lambda x: ((x.score_max_dpd + x.score_total_presence + x.score_excess_repayment +
                     x.score_sum_tr + x.score_total_amount_ppob + x.score_max_amount_saving +
-                    x.score_total_saving_frequency)/7)*10,
-                    axis=1
+                    x.score_total_saving_frequency)/7)*10,axis=1
                     )
 
     aggregate = df_score.groupby('mitra_score').agg({'label':'mean'}).reset_index()
